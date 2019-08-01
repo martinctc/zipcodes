7eeede28bc55703af50bbd9a492c08966e5edc2e
Martin Chan <martinchan53@gmail.com>
2019-08-01 18:47:42 +0100

    Add package files via upload

---
+++ R/uszips.R
@@ -0,0 +1,13 @@
+#' US Zip Codes Database from https://simplemaps.com/data/us-zips
+#'
+#' Basic US Zip Codes database compiled by simplemaps.
+#' Built using a combination of US Postal Service, US Census Bureau, National Weather Service, American Community Survey, and the IRS.
+#'
+#' @docType data
+#'
+#' @usage data(uszips)
+#'
+#' @references
+#' (\href{https://simplemaps.com/data/us-zips}{US Zip Codes Database})
+
+"uszips"
